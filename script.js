1f1b3ab19ce7ca24ccd2ed1d1d94d394624f50e9
Nitin Jangir <115369751+nitinjangir013@users.noreply.github.com>
2022-12-11 20:19:32 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+$(".main_container").fadeIn(3000).css('display','flex');
+$(".content_1").hover(function(){
+  $('.hover_1').animate({"width":"0px"});
+  }, function(){
+  $('.hover_1').animate({"width":"150px"});
+});
+
+$(".content_2").hover(function(){
+  $('.hover_2').animate({"width":"0px"});
+  }, function(){
+  $('.hover_2').animate({"width":"150px"});
+});
+
+$(".content_3").hover(function(){
+  $('.hover_3').animate({"width":"0px"});
+  }, function(){
+  $('.hover_3').animate({"width":"150px"});
+});
+
+$(".content_4").hover(function(){
+  $('.hover_4').animate({"width":"0px"});
+  }, function(){
+  $('.hover_4').animate({"width":"150px"});
+});
